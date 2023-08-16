336c052572432c349377a2bcfaf34f22e45f33f9
Hafiz Hamza <hafizhamxa8@gmail.com>
2023-08-16 08:05:43 +0500

    first commit

---
+++ apps.js
@@ -0,0 +1,50 @@
+// question = 4;
+// let num1 = +prompt("please enter your first number")
+// let num2 = +prompt("please enter your second number")
+// let operator = prompt("please enter your oprator")
+
+// function caculater(num1 ,operator, num2)
+// {
+
+//    switch (operator){
+     
+//      case "*" :
+//      let multiply = num1 * num2
+//      console.log(multiply)
+//      break;
+
+//      case "/" :
+//        let Divide = num1 / num2
+//      console.log(Divide)
+//      break;
+     
+//      case "+" :
+//        let sum = num1 + num2
+//        console.log(sum)
+//        break;
+
+//        case "-" :
+//          let sub = num1 - num2
+//          console.log(sub)
+//          break;
+         
+//          default:
+//            return"please your correct putting tha velue"; 
+
+//           }
+          
+//         }
+//         console.log(caculater(num1 ,operator, num2));
+
+// question = 5;
+
+// let number2 = +prompt('Enter value')
+// function numberOfSquair (number2 , number2)
+// {
+
+// const number = number2*number2
+
+// return number
+
+// }
+// console.log(numberOfSquair (number2 , number2));
